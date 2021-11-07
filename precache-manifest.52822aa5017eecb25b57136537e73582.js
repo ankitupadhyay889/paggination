bf3454833398ae640cb27209bfbb27553bb2bee9
Ankit <ankitupa7799@gmail.com>
2021-11-07 10:12:50 +0530

    Updates

---
+++ precache-manifest.52822aa5017eecb25b57136537e73582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d8a63ba4b57d1c8cb8b1c01219132d",
+    "url": "/paggination/index.html"
+  },
+  {
+    "revision": "d218815084aa6dfb7663",
+    "url": "/paggination/static/css/main.3c0b5a21.chunk.css"
+  },
+  {
+    "revision": "1a32dfe50a61adf7165a",
+    "url": "/paggination/static/js/2.626c51c8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/paggination/static/js/2.626c51c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d218815084aa6dfb7663",
+    "url": "/paggination/static/js/main.c85b6de3.chunk.js"
+  },
+  {
+    "revision": "5cbd798b4a4fc6f6880f",
+    "url": "/paggination/static/js/runtime-main.05114992.js"
+  }
+]);
